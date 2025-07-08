c1df367ce42b51b3436a05a1bf549cd046cf95b0
Christian Catelli <59646804+catellic@users.noreply.github.com>
2025-07-08 15:29:08 +0200

    adding ex6 solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, repeat) {
+    let wrd = "";
+    if (repeat >= 0) {
 
+        for (let i = 0; i < repeat; i++) {
+            wrd = wrd + word;
+        }
+        return wrd
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
